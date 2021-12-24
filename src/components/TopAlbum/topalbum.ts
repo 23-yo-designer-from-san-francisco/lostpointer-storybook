31b32fda9ed52d09336e06aa0df19dd109ba7ac9
Leonid <belyaevleonid2000@mail.ru>
2021-12-24 20:28:07 +0300

    Исправлена длина треков

--- src/components/TopAlbum/topalbum.ts
+++ src/components/TopAlbum/topalbum.ts
@@ -3,7 +3,6 @@ import { Component } from 'components/Component/component';
 import { IAlbum } from 'types/IAlbum';
 
 import AlbumTemplate from './topalbum.hbs';
-import '@fortawesome/fontawesome-free/css/all.min.css';
 import './topalbum.scss';
 
 export class TopAlbum<
